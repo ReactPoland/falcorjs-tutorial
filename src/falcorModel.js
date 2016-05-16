df3cb144f44510d18c4f8f83b234d65144e4e2c5
Michal Sawicki <michal@mobilewebpro.pl>
2016-05-16 10:50:50 +0200

    falcor model changed

--- src/falcorModel.js
+++ src/falcorModel.js
@@ -1,24 +1,11 @@
 const falcor = require('falcor');
 const FalcorDataSource = require('falcor-http-datasource');
-
-let cache = {
-  articles: [
-    {
-        id: 987654,
-        descriptionTitle: "First from falcor model title yo yo yo ",
-        descriptionContent: "Our from falcor model description content"
-    },
-    {
-        id: 123456,
-        descriptionTitle: "Second  from falcor model title",
-        descriptionContent: "Another from falcor model hi hi hi description content"
-    }
-  ]
-};
+const $ref = falcor.Model.ref;
+const $atom = falcor.Model.atom;
 
 
 const model = new falcor.Model({
-  "cache": cache
+  source: new FalcorDataSource('/model.json')
 });
 
 export default model;
