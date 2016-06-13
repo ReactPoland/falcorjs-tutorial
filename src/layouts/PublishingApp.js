143a58bfb7e7edf95a5be0dab8df73c8d13d8422
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-13 14:55:44 +0200

    CODE - app.js and publishingapp.js files changes according to book

--- src/layouts/PublishingApp.js
+++ src/layouts/PublishingApp.js
@@ -41,15 +41,15 @@ class PublishingApp extends React.Component {
 
   render () {
   	let articlesJSX = [];
-  	for(let articleKey in this.props) {
-  		let articleDetails = this.props[articleKey];
-  		let currentArticleJSX = (
-  			<div key={articleKey}>
-  				<h2>{articleDetails.articleTitle}</h2>
-  				<h3>{articleDetails.articleContent}</h3>
-  			</div>);
-  		articlesJSX.push(currentArticleJSX);
-  	}
+    for(let articleKey in this.props.article) {
+      let articleDetails = this.props.article[articleKey];
+      let currentArticleJSX = (
+        <div key={articleKey}>
+          <h2>{articleDetails.articleTitle}</h2>
+          <h3>{articleDetails.articleContent}</h3>
+        </div>);
+      articlesJSX.push(currentArticleJSX);
+    }
     return (
       <div>
           <h1>Our publishing app</h1>
