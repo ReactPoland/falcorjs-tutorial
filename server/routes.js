8b2f470b86a77485df0c57dc57220f24f8c86233
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-13 12:41:38 +0200

    CODE - app.use(bodyParser.urlencoded({extended: false})); in server.js and correct import of Article in routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,11 @@
-import mongoose from 'mongoose';
+/*import { Article } from './configMongoose';*/
+import configMongoose from './configMongoose.js';
+let Article = configMongoose.Article;
+import sessionRoutes from './routesSession';
 
-mongoose.connect('mongodb://localhost/local');
-
-var articleSchema = {
-  articleTitle:String,
-  articleContent:String
-}
-
-var Article = mongoose.model('Article', articleSchema, 'articles');
-
-let PublishingAppRoutes = [{
+let PublishingAppRoutes = [
+   ...sessionRoutes,
+{
   route: 'articles.length',
     get: () => {
       return Article.count({}, function(err, count) {
