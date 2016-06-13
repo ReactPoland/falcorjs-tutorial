9bbf904d592d2f002179a822cc17f01c97970f55
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-13 13:42:57 +0200

    CODE - npm i jwt and routesSession added new code from the book

--- server/routesSession.js
+++ server/routesSession.js
@@ -1,16 +1,62 @@
+import { User } from './configMongoose';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import jwtSecret from './configSecret';
+
 export default [
   { 
     route: ['login'] ,
     call: (callPath, args) => 
       {
         let { username, password } = args[0];
-
+        let saltedPassword = password+"pubApp"; // pubApp is our salt string
+        let saltedPassHash = crypto.createHash('sha256').update(saltedPassword).digest('hex');
         let userStatementQuery = {
           $and: [
               { 'username': username },
-              { 'password': password }
+              { 'password': saltedPassHash }
           ]
         }
+        return User.find(userStatementQuery, function(err, user) {
+          if (err) throw err;
+        }).then((result) => {
+          if(result.length) {
+            let role = result[0].role;
+            let userDetailsToHash = username+role;
+            let token = jwt.sign(userDetailsToHash, jwtSecret.secret);
+            return [
+              {
+                path: ['login', 'token'],
+                value: token
+              },
+              {
+                path: ['login', 'username'],
+                value: username
+              },
+              {
+                path: ['login', 'role'],
+                value: role
+              },
+              {
+                path: ['login', 'error'],
+                value: false
+              }
+            ];
+          } else {
+            // INVALID LOGIN
+            return [
+              {
+                path: ['login', 'token'], 
+                value: "INVALID"
+              },
+              {
+                path: ['login', 'error'], 
+                value: "NO USER FOUND, incorrect login information" 
+              }
+            ];
+          }
+          return result;
+        });
       }
   }
 ];
