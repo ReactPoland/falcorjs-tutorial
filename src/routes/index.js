52e11d93224cab03a7ff4b28f36b4ca529484fcb
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-14 10:19:34 +0200

    CODE - added dashboardview component and inserted it to the routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,17 @@ import { Route, IndexRoute }        from 'react-router';
 import CoreLayout                   from '../layouts/CoreLayout';
 
 /* home view */
-import PublishingApp                    from '../layouts/PublishingApp';
+import PublishingApp                from '../layouts/PublishingApp';
 
 /* auth views */
 import LoginView                    from '../views/LoginView';
+
+import DashboardView 				from '../views/DashboardView';
 
 export default (
   <Route component={CoreLayout} path='/'>
     <IndexRoute component={PublishingApp} name='home' />
     <Route component={LoginView} path='login' name='login' />
+    <Route component={DashboardView} path='dashboard' name='dashboard' />
   </Route>
 );
