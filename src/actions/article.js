b2f5f977b1a4efd093f0dd30c285a25a892a5f15
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-23 16:44:24 +0200

    CODE - added new action pushNewArticle

--- src/actions/article.js
+++ src/actions/article.js
@@ -4,5 +4,12 @@ export default {
       type: 'ARTICLES_LIST_ADD',
       payload: { response: response }
     }
+  },
+  pushNewArticle: (response) => {
+    return {
+      type: 'PUSH_NEW_ARTICLE',
+      payload: { response: response }
+    }
   }
+
 }
