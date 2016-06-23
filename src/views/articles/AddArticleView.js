ba5b126377a34ba8bcbc709c9e8d2a53d2b2a3d8
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-23 15:08:56 +0200

    CODE - added some dependencies and modified AddArticleView.js

--- src/views/articles/AddArticleView.js
+++ src/views/articles/AddArticleView.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import WYSWIGeditor from '../../components/articles/WYSWIGeditor.js';
+import { stateToHTML } from 'draft-js-export-html';
 
 const mapStateToProps = (state) => ({
   ...state
@@ -15,13 +16,26 @@ const mapDispatchToProps = (dispatch) => ({
 class AddArticleView extends React.Component {
   constructor(props) {
     super(props);
+    this._onDraftJSChange = this._onDraftJSChange.bind(this);
+
+    this.state = {
+      contentJSON: {},
+      htmlContent: ''
+    };
+  }
+  _onDraftJSChange(contentJSON, contentState) {
+    let htmlContent = stateToHTML(contentState);
+    this.setState({contentJSON, htmlContent});
   }
 
-  render () {
+   render () {
     return (
       <div style={{height: '100%', width: '75%', margin: 'auto'}}>
         <h1>Add Article</h1>
-        <WYSWIGeditor />
+        <WYSWIGeditor
+          initialValue=''
+          title="Create an article"
+          onChangeTextJSON={this._onDraftJSChange} />
       </div>
     );
   }
