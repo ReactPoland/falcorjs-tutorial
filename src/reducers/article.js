65444b33caac4d5a7434db0c82095d4e5e321ac9
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-23 17:01:20 +0200

    CODE - reducer modified using maphelpers now

--- src/reducers/article.js
+++ src/reducers/article.js
@@ -1,13 +1,16 @@
+import mapHelpers from '../utils/mapHelpers';
+
 const article = (state = {}, action) => {
-	switch (action.type) {
-		case 'RETURN_ALL_ARTICLES':
-			return Object.assign({}, state);
-		case 'ARTICLES_LIST_ADD':
-			let articlesList = action.payload.response;
-			return Object.assign({}, articlesList);
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'ARTICLES_LIST_ADD':
+      let articlesList = action.payload.response;
+      return mapHelpers.addMultipleItems(state, articlesList);
+    case 'PUSH_NEW_ARTICLE':
+      let newArticleObject = action.payload.response;
+      return mapHelpers.addItem(state, newArticleObject['_id'], newArticleObject);
+    default:
+      return state;
+  }
 }
 
 export default article
