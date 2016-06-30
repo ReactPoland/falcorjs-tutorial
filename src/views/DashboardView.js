aca2f623b8bdb1a1feb6edb2397d024104e7da91
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-30 12:28:25 +0200

    CODE - modified code in dashboard js

--- src/views/DashboardView.js
+++ src/views/DashboardView.js
@@ -31,18 +31,20 @@ class DashboardView extends React.Component {
   render () {
 
     let articlesJSX = [];
-    this.props.article.forEach((articleDetails, articleKey) => {
-      let currentArticleJSX = (
+  this.props.article.forEach((articleDetails, articleKey) => {
+    let currentArticleJSX = (
+      <Link to={`/edit-article/${articleDetails['_id']}`}>
         <ListItem
           key={articleKey}
           leftAvatar={<img src="/static/placeholder.png" width="50" height="50" />}
           primaryText={articleDetails.articleTitle}
           secondaryText={articleDetails.articleContent}
         />
-      );
+      </Link>
+    );
 
-      articlesJSX.push(currentArticleJSX);
-    });
+    articlesJSX.push(currentArticleJSX);
+  });
     return (
       <div style={{height: '100%', width: '75%', margin: 'auto'}}>
         <Link to='/add-article'>
