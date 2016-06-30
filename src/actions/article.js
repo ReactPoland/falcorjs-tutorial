49f66659bb8f350c296f21b6f962c2debc4532df
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-30 12:25:40 +0200

    CODE - added new actions

--- src/actions/article.js
+++ src/actions/article.js
@@ -10,6 +10,18 @@ export default {
       type: 'PUSH_NEW_ARTICLE',
       payload: { response: response }
     }
+  },
+  editArticle: (response) => {
+    return {
+      type: 'EDIT_ARTICLE',
+      payload: { response: response }
+    }
+  },
+  deleteArticle: (response) => {
+    return {
+      type: 'DELETE_ARTICLE',
+      payload: { response: response }
+    }
   }
 
 }
