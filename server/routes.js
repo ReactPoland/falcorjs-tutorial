644206bc7ca31f3dab21f2171ed395d191d0a257
Michal Sawicki <michal@mobilewebpro.pl>
2016-07-01 13:49:40 +0200

    CODE - ne server/routes.js code, chapter 5 starting

--- server/routes.js
+++ server/routes.js
@@ -1,44 +1,59 @@
-/*import { Article } from './configMongoose';*/
-import configMongoose from './configMongoose.js';
-let Article = configMongoose.Article;
+import configMongoose from './configMongoose';
 import sessionRoutes from './routesSession';
+import jsonGraph from 'falcor-json-graph';
+import jwt from 'jsonwebtoken';
+import jwtSecret from './configSecret';
 
-let PublishingAppRoutes = [
-   ...sessionRoutes,
-{
-  route: 'articles.length',
-    get: () => {
-      return Article.count({}, function(err, count) {
-        return count;
-      }).then ((articlesCountInDB) => {
-        return {
-          path: ['articles', 'length'],
-          value: articlesCountInDB
-        }
+let $atom = jsonGraph.atom; // this will be explained later in that chapter
+let Article = configMongoose.Article;
+
+export default ( req, res ) => {
+  let { token, role, username } = req.headers;
+  let userDetailsToHash = username+role;
+  let authSignToken = jwt.sign(userDetailsToHash, jwtSecret.secret);
+  let isAuthorized = authSignToken === token;
+  let sessionObject = {isAuthorized, role, username};
+
+  console.info(`The ${username} is authorized === `, isAuthorized);
+
+  let PublishingAppRoutes = [
+      ...sessionRoutes,
+    {
+    route: 'articles.length',
+      get: () => {
+        return Article.count({}, function(err, count) {
+          return count;
+        }).then ((articlesCountInDB) => {
+          return {
+            path: ['articles', 'length'],
+            value: articlesCountInDB
+          }
+        })
+    }
+  },{
+    route: 'articles[{integers}]["_id","articleTitle","articleContent"]',
+    get: (pathSet) => {
+      let articlesIndex = pathSet[1];
+
+      return Article.find({}, function(err, articlesDocs) {
+        return articlesDocs;
+      }).then ((articlesArrayFromDB) => {
+        let results = [];
+        articlesIndex.forEach((index) => {
+          let singleArticleObject = articlesArrayFromDB[index].toObject();
+
+          let falcorSingleArticleResult = {
+            path: ['articles', index],
+            value: singleArticleObject
+          };
+
+          results.push(falcorSingleArticleResult);
+        });
+        return results;
       })
-  }
-}, 
-{
-  route: 'articles[{integers}]["_id","articleTitle","articleContent"]',
-  get: (pathSet) => {
-    let articlesIndex = pathSet[1];
+    }
+  }];
 
-    return Article.find({}, function(err, articlesDocs) {
-      return articlesDocs;
-    }).then ((articlesArrayFromDB) => {
-      let results = [];
-      articlesIndex.forEach((index) => {
-        let singleArticleObject = articlesArrayFromDB[index].toObject();
-        let falcorSingleArticleResult = {
-          path: ['articles', index],
-          value: singleArticleObject
-        };
-        results.push(falcorSingleArticleResult);
-      });
-      /*console.info(">>>> results", results);*/
-      return results;
-    })
-  }
-}];
 
-export default PublishingAppRoutes;
+  return PublishingAppRoutes;
+}
