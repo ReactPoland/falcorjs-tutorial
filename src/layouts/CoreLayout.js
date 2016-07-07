b0e8d8d2930eefc963d60ea65c1236db2a57cfc9
przeor <kamil@przeorski.pl>
2016-07-07 12:19:09 +0200

    hotfix mui provider with connect

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -5,12 +5,29 @@ import themeDecorator from 'material-ui/lib/styles/theme-decorator';
 import RaisedButton from 'material-ui/lib/raised-button';
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import articleActions from '../actions/article.js';
+
+const mapStateToProps = (state) => ({
+  ...state
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  articleActions: bindActionCreators(articleActions, dispatch)
+});
+
 class MyComponent extends React.Component {
  render() {
+    console.info(this.props);
     return (
       <div>Hello world<RaisedButton label="Default" /></div>
     );
   }
 }
 
-export default themeDecorator(getMuiTheme(null, { userAgent: 'all' }))(MyComponent);
+
+const muiCoreLayout = themeDecorator(getMuiTheme(null, { userAgent: 'all' }))(MyComponent);
+
+export default connect(mapStateToProps, mapDispatchToProps)(muiCoreLayout);
